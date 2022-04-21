152bd91f5e40e5dbd08476a402983c8d2d398907
Julien Polo <julien.polo@koober.com>
2022-04-21 23:13:18 +0200

    refactor: remove data file

--- packages/fs/src/data.ts
+++
@@ -1,3 +0,0 @@
-import type * as fs from 'node:fs';
-
-export type FileStats = fs.Stats;
--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -1,4 +1,3 @@
 export * from './system.js';
-export * from './data.js';
 export * from './path.js';
 export * from './error.js';
