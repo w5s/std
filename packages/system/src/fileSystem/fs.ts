857795cfe4dcd8690c8208b4ed10202c8f4056b2
Julien Polo <julien.polo@koober.com>
2022-05-15 22:09:06 +0200

    docs: add documentation to functions

--- packages/system/src/fileSystem/fs.ts
+++ packages/system/src/fileSystem/fs.ts
@@ -4,6 +4,17 @@ import { FileError } from '../error';
 import { Internal, errnoTask, errnoExceptionHandler } from '../internal';
 import { FilePath } from '../path';
 
+/**
+ * Reads the contents of a directory.
+ *
+ * @example
+ * ```ts
+ * const task = listDirectory(FilePath('/path/to/directory'));
+ * const list = await Task.unsafeRun(task); // Result.Ok([FilePath('file1'), FilePath('file2')])
+ * ```
+ * @param filePath - The path to the directory.
+ * @param options - The options to use.
+ */
 export function listDirectory(filePath: FilePath, options?: listDirectory.Options): Task<Array<FilePath>, FileError> {
   // @ts-ignore - `readdir` returns an array of strings instead of a Array<FilePath>
   return errnoTask<[path: nodeFS.PathLike, options?: BufferEncoding], string[]>(Internal.FS.readdir)(filePath, options);
@@ -12,6 +23,17 @@ export namespace listDirectory {
   export type Options = BufferEncoding;
 }
 
+/**
+ * Asynchronously creates a directory.
+ *
+ * @example
+ * ```ts
+ * const task = createDirectory(FilePath('/path/to/directory'));
+ * await Task.unsafeRun(task); // Will create the directory
+ * ```
+ * @param path - The path to the directory.
+ * @param options - The options to use.
+ */
 export function createDirectory(path: FilePath, options?: createDirectory.Options): Task<void, FileError> {
   return Task.map(errnoTask(Internal.FS.mkdir)(path, options), ignore);
 }
@@ -27,14 +49,47 @@ export const stat = errnoTask<[pathLike: nodeFS.PathLike], nodeFS.Stats>((pathLi
 );
 export const symlink = errnoTask(Internal.FS.symlink);
 
+/**
+ * Asynchronously copies `source` to `destination`. By default, `destination` is overwritten if it already exists.
+ *
+ * @example
+ * ```ts
+ * const task = copyFile(FilePath('/path/to/source'), FilePath('/path/to/destination'));
+ * await Task.unsafeRun(task); // Will copy the file
+ * ```
+ * @param source - The source path.
+ * @param destination - The destination path.
+ */
 export function copyFile(source: FilePath, destination: FilePath): Task<void, FileError> {
   return errnoTask(Internal.FS.copyFile)(source, destination);
 }
 
+/**
+ * Renames `oldPath` to `newPath`.
+ *
+ * @example
+ * ```ts
+ * const task = rename(FilePath('/path/to/old'), FilePath('/path/to/new'));
+ * await Task.unsafeRun(task); // Will rename the file
+ * ```
+ * @param oldPath - The path to the file to be renamed.
+ * @param newPath - The path to the new file.
+ */
 export function rename(oldPath: FilePath, newPath: FilePath): Task<void, FileError> {
   return errnoTask(Internal.FS.rename)(oldPath, newPath);
 }
 
+/**
+ * Removes files and directories (modeled on the standard POSIX `rm` utility).
+ *
+ * @example
+ * ```ts
+ * const task = remove(FilePath('/path/to/file.txt'));
+ * Task.unsafeRun(task);
+ * ```
+ * @param filePath - The path to the file to be removed.
+ * @param options - The options to be used when removing the file.
+ */
 export function remove(filePath: FilePath, options?: remove.Options): Task<void, FileError> {
   return errnoTask(Internal.FS.rm)(filePath, options);
 }
