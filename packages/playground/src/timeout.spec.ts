7cc53d05cdf0c6aae7694ec520d89e7d3da76872
Julien Polo <julien.polo@koober.com>
2022-05-29 00:11:32 +0200

    chore: add tests and docs to timeout experiment

--- packages/playground/src/timeout.spec.ts
+++ packages/playground/src/timeout.spec.ts
@@ -1,7 +1,64 @@
-import { timeout } from './timeout';
+import { Ref, Result, Task, TimeDuration } from '@w5s/core';
+import { timeout, TimeoutError } from './timeout';
 
 describe(timeout, () => {
-  test('should be defined', () => {
-    expect(timeout).toBeDefined();
+  const anyDelay = TimeDuration.milliseconds(4);
+  const anyValue = 'any_value';
+  const anyError = 'any_error';
+
+  test('should resolve/reject the same value as task', async () => {
+    const resolveNow = Task.resolve(anyValue);
+    const resolved = timeout(resolveNow, anyDelay);
+    expect(Task.unsafeRun(resolved)).toEqual(Result.Ok(anyValue));
+
+    const rejectNow = Task.reject(anyError);
+    const rejected = timeout(rejectNow, anyDelay);
+    expect(Task.unsafeRun(rejected)).toEqual(Result.Error(anyError));
+  });
+  test('should cancel task and setTimeout if task is canceled', async () => {
+    const clearTimeoutSpy = jest.spyOn(globalThis, 'clearTimeout');
+    clearTimeoutSpy.mockClear();
+    const canceler = jest.fn();
+    const canceled = Task.wrap((_resolve, _reject, cancelerRef) => {
+      cancelerRef.current = canceler;
+    });
+    const task = timeout(canceled, anyDelay);
+    const cancelerRef = Ref(canceler);
+    task[Task.run](
+      () => {},
+      () => {},
+      cancelerRef
+    );
+    cancelerRef.current();
+    expect(canceler).toHaveBeenCalled();
+    expect(clearTimeoutSpy).toHaveBeenCalled();
+  });
+  test('should cancel task if timeout is triggered', async () => {
+    const canceler = jest.fn();
+    const willCancel = Task.wrap((_resolve, _reject, cancelerRef) => {
+      cancelerRef.current = canceler;
+    });
+    const task = timeout(willCancel, anyDelay);
+    await expect(Task.unsafeRun(task)).resolves.toEqual(
+      Result.Error(
+        TimeoutError({
+          message: `Task timed out after ${anyDelay}ms`,
+          delay: anyDelay,
+        })
+      )
+    );
+    expect(canceler).toHaveBeenCalled();
+  });
+  test('should cancel timeout if task is resolved', async () => {
+    const clearTimeoutSpy = jest.spyOn(globalThis, 'clearTimeout');
+    clearTimeoutSpy.mockClear();
+    const resolved = timeout(Task.resolve(anyValue), anyDelay);
+    await Task.unsafeRun(resolved);
+    expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
+
+    clearTimeoutSpy.mockClear();
+    const rejected = timeout(Task.reject(anyError), anyDelay);
+    await Task.unsafeRun(rejected);
+    expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
   });
 });
--- packages/playground/src/timeout.ts
+++ packages/playground/src/timeout.ts
@@ -1,8 +1,21 @@
 import { DataError, Ref, Task, TimeDuration } from '@w5s/core';
 
-export interface TimeoutError extends DataError<{ name: 'TimeoutError' }> {}
+export interface TimeoutError extends DataError<{ name: 'TimeoutError'; delay: TimeDuration }> {}
 export const TimeoutError = DataError.Make<TimeoutError>('TimeoutError');
 
+/**
+ * Creates a task that will reject a {@link TimeoutError} if `task` is not resolved or rejected within `delay`
+ *
+ * @example
+ * ```ts
+ * const longTask = Task.delay(TimeDuration.hours(1));
+ * const timeoutTask = timeout(longTask, TimeDuration.milliseconds(4));
+ * Task.unsafeRun(timeoutTask); // Result.Error(TimeoutError({ message: 'Task timed out after 4ms', delay: 4 }))
+ * // Note that `longTask` will be canceled and will never resolve nor reject
+ * ```
+ * @param task - task to cancel after delay
+ * @param delay - delay in milliseconds
+ */
 export function timeout<Value, Error>(
   task: Task<Value, Error>,
   delay: TimeDuration
@@ -16,7 +29,12 @@ export function timeout<Value, Error>(
 
     const timeoutId = setTimeout(() => {
       taskCancel();
-      reject(TimeoutError({}));
+      reject(
+        TimeoutError({
+          message: `Task timed out after ${stringifyDelay(delay)}`,
+          delay,
+        })
+      );
     }, delay);
     const timeoutCancel = () => clearTimeout(timeoutId);
 
@@ -37,4 +55,8 @@ export function timeout<Value, Error>(
       taskCancelerRef
     );
   });
+}
+
+function stringifyDelay(delay: TimeDuration) {
+  return `${String(delay)}ms`;
 }
