af615a20bb77d7687bba165c48846f36a58c9f32
Julien Polo <julien.polo@koober.com>
2022-06-07 23:01:15 +0200

    docs: add documentation to ensure functions

--- packages/system/src/fileSystem/ensure.ts
+++ packages/system/src/fileSystem/ensure.ts
@@ -5,6 +5,16 @@ import { readSymbolicLinkStatus, createDirectory, createSymbolicLink, writeFile 
 
 type FileType = 'file' | 'directory' | 'symlink';
 
+/**
+ * Ensures that the directory exists. If the directory structure does not exist, it is created.
+ *
+ * @example
+ * ```ts
+ * const task = ensureDirectory(FilePath('/path/to/directory'));
+ * await Task.unsafeRun(task);// Will create a directory
+ * ```
+ * @param filePath - The path to ensure
+ */
 export function ensureDirectory(filePath: FilePath): Task<void, FileError> {
   return Task.andThen(linkStat(filePath), (linkType) =>
     Option.isNone(linkType)
@@ -13,6 +23,16 @@ export function ensureDirectory(filePath: FilePath): Task<void, FileError> {
   );
 }
 
+/**
+ * Ensures that the file exists. If the file that is requested to be created is in directories that do not exist, these directories are created. If the file already exists, it is NOT MODIFIED.
+ *
+ * @example
+ * ```ts
+ * const task = ensureFile(FilePath('/path/to/file'));
+ * await Task.unsafeRun(task);// Will create a file
+ * ```
+ * @param filePath - The path to ensure
+ */
 export function ensureFile(filePath: FilePath): Task<void, FileError> {
   return Task.andThen(linkStat(filePath), (linkType) =>
     Option.isNone(linkType)
@@ -21,6 +41,17 @@ export function ensureFile(filePath: FilePath): Task<void, FileError> {
   );
 }
 
+/**
+ * Ensures that the symlink exists. If the directory structure does not exist, it is created.
+ *
+ * @example
+ * ```ts
+ * const task = ensureSymbolicLink(FilePath('/path/to/file'), FilePath('/path/to/link'));
+ * await Task.unsafeRun(task);// Will create a symbolic link
+ * ```
+ * @param source - The source path
+ * @param destination - The destination path
+ */
 export function ensureSymbolicLink(source: FilePath, destination: FilePath): Task<void, FileError> {
   return Task.andThen(linkStat(destination), (destinationLinkType) =>
     Option.isNone(destinationLinkType)
