e587d3073428a11bc2d8b1fbea353fde2832ad09
Julien Polo <julien.polo@koober.com>
2022-06-08 23:17:28 +0200

    feat: add Result.match

--- packages/core/src/result.spec.ts
+++ packages/core/src/result.spec.ts
@@ -183,4 +183,22 @@ describe('Result', () => {
       });
     });
   });
+  describe(Result.match, () => {
+    test('should call matchers.Ok when Ok', () => {
+      expect(
+        Result.match(Result.Ok('ok'), {
+          Ok: (value) => `${value}_value`,
+          Error: (error) => `${error}_error`,
+        })
+      ).toEqual('ok_value');
+    });
+    test('should call matchers.Error when Error', () => {
+      expect(
+        Result.match(Result.Error('error'), {
+          Ok: (value) => `${value}_value`,
+          Error: (error) => `${error}_error`,
+        })
+      ).toEqual('error_error');
+    });
+  });
 });
