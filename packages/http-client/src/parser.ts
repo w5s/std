6602eec0c888dbb3726dac99f22988e663f3ff00
Julien Polo <julien.polo@koober.com>
2022-06-10 19:25:59 +0200

    refactor: use only import type for tag

--- packages/http-client/src/parser.ts
+++ packages/http-client/src/parser.ts
@@ -1,29 +1,41 @@
-import { JSONValue, Task } from '@w5s/core';
+import type { JSONValue, Task } from '@w5s/core';
 import { HTTPClient } from './client.js';
 
-function handleError(error: unknown) {
-  return HTTPClient.ParserError({
-    cause: error,
-  });
+function tryCall<V>(fn: () => Promise<V>): Task<V, HTTPClient.ParserError> {
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    'Task/run': async (resolve, reject) => {
+      try {
+        resolve(await fn());
+      } catch (error: unknown) {
+        reject(
+          HTTPClient.ParserError({
+            cause: error,
+          })
+        );
+      }
+    },
+  };
 }
 
 export function parseArrayBuffer(response: HTTPClient.Response) {
-  return Task.tryCall<ArrayBuffer, HTTPClient.ParserError>(() => response.arrayBuffer(), handleError);
+  return tryCall<ArrayBuffer>(() => response.arrayBuffer());
 }
 
 export function parseJSON<Return extends JSONValue>(_decode: 'unsafe') {
-  return (response: HTTPClient.Response) =>
-    Task.tryCall<Return, HTTPClient.ParserError>(() => response.json(), handleError);
+  return (response: HTTPClient.Response): Task<Return, HTTPClient.ParserError> =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    tryCall(() => response.json());
 }
 
-export function parseBlob(response: HTTPClient.Response) {
-  return Task.tryCall<Blob, HTTPClient.ParserError>(() => response.blob(), handleError);
+export function parseBlob(response: HTTPClient.Response): Task<Blob, HTTPClient.ParserError> {
+  return tryCall(() => response.blob());
 }
 
-export function parseText(response: HTTPClient.Response) {
-  return Task.tryCall<string, HTTPClient.ParserError>(() => response.text(), handleError);
+export function parseText(response: HTTPClient.Response): Task<string, HTTPClient.ParserError> {
+  return tryCall(() => response.text());
 }
 
-export function parseFormData(response: HTTPClient.Response) {
-  return Task.tryCall<FormData, HTTPClient.ParserError>(() => response.formData(), handleError);
+export function parseFormData(response: HTTPClient.Response): Task<FormData, HTTPClient.ParserError> {
+  return tryCall(() => response.formData());
 }
