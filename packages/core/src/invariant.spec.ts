4888124af76e7a56d31a8444f8d7cedbd91445df
Julien Polo <julien.polo@koober.com>
2022-08-09 22:47:59 +0200

    ♻️ Move assertNever to invariant

--- packages/core/src/invariant.spec.ts
+++ packages/core/src/invariant.spec.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { describe, test, expect, jest } from '@jest/globals';
 import { assertType } from './assert.js';
-import { invariant, warning } from './invariant.js';
+import { invariant, warning, assertNever } from './invariant.js';
 
 describe(invariant, () => {
   const getThrownError = (fn: () => void) => {
@@ -51,4 +52,39 @@ describe(warning, () => {
     warning(false, undefined);
     expect(printSpy).toHaveBeenCalledWith('Warning: ');
   });
+});
+describe(assertNever, () => {
+  const anyValue = undefined;
+  const neverValue: never = undefined as never;
+
+  test('should report ts error', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    () => {
+      // @ts-expect-error anyValue is not never
+      assertNever(anyValue);
+    };
+  });
+
+  /**
+   * Type check
+   */
+  const typeTestExhaustive = (fruit: 'banana' | 'kiwi'): string => {
+    switch (fruit) {
+      case 'banana':
+        return '🍌 Banana';
+      case 'kiwi':
+        return '🥝 Kiwi';
+      default:
+        return assertNever(fruit);
+    }
+  };
+  const typeTestNonExhaustive = (fruit: 'banana' | 'kiwi'): string => {
+    switch (fruit) {
+      case 'banana':
+        return '🍌 Banana';
+      default:
+        // @ts-expect-error anyValue is not never
+        return assertNever(fruit);
+    }
+  };
 });
