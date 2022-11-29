4c766417cfeb6b8f82bef61664e15ae27dd76c36
Julien Polo <julien.polo@gmail.com>
2022-11-29 09:06:51 +0100

    ✅  Improve describe block for disposable

--- packages/core/src/disposable.spec.ts
+++ packages/core/src/disposable.spec.ts
@@ -3,28 +3,32 @@ import { Disposable, AsyncDisposable } from './disposable.js';
 import { Symbol } from './symbol.js';
 
 describe('Disposable', () => {
-  it.each([
-    [{ [Symbol.dispose]: () => {} }, true],
-    [{ [Symbol.asyncDispose]: () => {} }, false],
-    [{ [Symbol.dispose]: true }, false],
-    [{}, false],
-    ['string', false],
-    [null, false],
-    [undefined, false],
-  ])('should return true for { [Symbol.dispose] }', (object, expected) => {
-    expect(Disposable.hasInstance(object)).toEqual(expected);
+  describe(Disposable.hasInstance, () => {
+    it.each([
+      [{ [Symbol.dispose]: () => {} }, true],
+      [{ [Symbol.asyncDispose]: () => {} }, false],
+      [{ [Symbol.dispose]: true }, false],
+      [{}, false],
+      ['string', false],
+      [null, false],
+      [undefined, false],
+    ])('should return true for { [Symbol.dispose] }', (object, expected) => {
+      expect(Disposable.hasInstance(object)).toEqual(expected);
+    });
   });
 });
 describe('AsyncDisposable', () => {
-  it.each([
-    [{ [Symbol.dispose]: () => {} }, false],
-    [{ [Symbol.asyncDispose]: () => {} }, true],
-    [{ [Symbol.dispose]: true }, false],
-    [{}, false],
-    ['string', false],
-    [null, false],
-    [undefined, false],
-  ])('should return true for { [Symbol.asyncDispose] }', (object, expected) => {
-    expect(AsyncDisposable.hasInstance(object)).toEqual(expected);
+  describe(AsyncDisposable.hasInstance, () => {
+    it.each([
+      [{ [Symbol.dispose]: () => {} }, false],
+      [{ [Symbol.asyncDispose]: () => {} }, true],
+      [{ [Symbol.dispose]: true }, false],
+      [{}, false],
+      ['string', false],
+      [null, false],
+      [undefined, false],
+    ])('should return true for { [Symbol.asyncDispose] }', (object, expected) => {
+      expect(AsyncDisposable.hasInstance(object)).toEqual(expected);
+    });
   });
 });
