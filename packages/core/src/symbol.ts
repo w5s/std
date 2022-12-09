7f55359f87e7a4ac0b5a2f555266055b59b17b66
Julien Polo <julien.polo@gmail.com>
2022-12-09 10:02:19 +0100

    ♻️ Refactor Symbol module

--- packages/core/src/symbol.ts
+++ packages/core/src/symbol.ts
@@ -3,37 +3,33 @@
 
 declare global {
   interface SymbolConstructor {
+    /**
+     * Disposes of resources within this object.
+     */
     readonly dispose: unique symbol;
+
+    /**
+     * Disposes asynchronously of resources within this object.
+     */
     readonly asyncDispose: unique symbol;
   }
 }
+const GlobalSymbol = globalThis.Symbol;
 
-export namespace Symbol {
-  const GlobalSymbol = globalThis.Symbol;
-
-  export const {
-    iterator,
-    asyncIterator,
-    hasInstance,
-    isConcatSpreadable,
-    match,
-    replace,
-    search,
-    species,
-    split,
-    toStringTag,
-    toPrimitive,
-    unscopables,
-  } = globalThis.Symbol;
-
-  /**
-   * Disposes of resources within this object.
-   */
-  export const dispose: typeof GlobalSymbol.dispose = GlobalSymbol.dispose ?? (GlobalSymbol.for('dispose') as unknown);
-
-  /**
-   * Disposes asynchronously of resources within this object.
-   */
-  export const asyncDispose: typeof GlobalSymbol.asyncDispose =
-    GlobalSymbol.asyncDispose ?? (GlobalSymbol.for('asyncDispose') as unknown);
-}
+export const Symbol: Omit<SymbolConstructor, 'keyFor' | 'for' | 'prototype'> = {
+  iterator: GlobalSymbol.iterator,
+  asyncIterator: GlobalSymbol.asyncIterator,
+  hasInstance: GlobalSymbol.hasInstance,
+  isConcatSpreadable: GlobalSymbol.isConcatSpreadable,
+  match: GlobalSymbol.match,
+  replace: GlobalSymbol.replace,
+  search: GlobalSymbol.search,
+  species: GlobalSymbol.species,
+  split: GlobalSymbol.split,
+  toStringTag: GlobalSymbol.toStringTag,
+  toPrimitive: GlobalSymbol.toPrimitive,
+  unscopables: GlobalSymbol.unscopables,
+  matchAll: GlobalSymbol.matchAll,
+  dispose: GlobalSymbol.dispose ?? (GlobalSymbol.for('dispose') as any),
+  asyncDispose: GlobalSymbol.asyncDispose ?? (GlobalSymbol.for('asyncDispose') as any),
+};
