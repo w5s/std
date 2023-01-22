05fd34df659c068d00541e1ca3f1272b934982f1
Julien Polo <julien.polo@gmail.com>
2023-01-22 15:51:08 +0100

    🚨 Correct lint error

--- jest.settings.cjs
+++ jest.settings.cjs
@@ -1,8 +1,7 @@
 // @ts-check
 
 /**
- * @template T
- * @type {(value: T[]|T|undefined) => T[]}
+ * @type {<T>(value: T[]|T|undefined) => T[]}
  */
 function toArray(value) {
   if (value == null) {
@@ -15,8 +14,7 @@ function toArray(value) {
 }
 
 /**
- * @template T
- * @type {(left: T[]|T|undefined, right:T[]|T|undefined) => T[]}
+ * @type {<T>(left: T[]|T|undefined, right:T[]|T|undefined) => T[]}
  */
 function concatArray(left, right) {
   return toArray(left).concat(toArray(right));
@@ -27,20 +25,14 @@ function concatArray(left, right) {
 /** @type {(...configs: JestConfig[]) => JestConfig} */
 function concatJestConfig(...configs) {
   return configs.reduce(
-    (
-      /** @type {JestConfig} */ returnValue,
-      /** @type {JestConfig} */ config
-    ) => ({
+    (/** @type {JestConfig} */ returnValue, /** @type {JestConfig} */ config) => ({
       ...returnValue,
       ...config,
       coveragePathIgnorePatterns: concatArray(
         returnValue.coveragePathIgnorePatterns,
         config.coveragePathIgnorePatterns
       ),
-      testPathIgnorePatterns: concatArray(
-        returnValue.testPathIgnorePatterns,
-        config.testPathIgnorePatterns
-      ),
+      testPathIgnorePatterns: concatArray(returnValue.testPathIgnorePatterns, config.testPathIgnorePatterns),
     }),
     {}
   );
@@ -50,23 +42,9 @@ function concatJestConfig(...configs) {
 function defaultJestConfig(config) {
   return concatJestConfig(
     {
-      coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '/docs/',
-        '/lib/',
-        '/build/',
-        '/.cache/',
-        '/public/',
-      ],
+      coveragePathIgnorePatterns: ['/node_modules/', '/docs/', '/lib/', '/build/', '/.cache/', '/public/'],
       preset: 'es-jest',
-      testPathIgnorePatterns: [
-        '/node_modules/',
-        '/lib/',
-        '/build/',
-        '/.cache/',
-        '/docs/',
-        '/public/',
-      ],
+      testPathIgnorePatterns: ['/node_modules/', '/lib/', '/build/', '/.cache/', '/docs/', '/public/'],
       moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
       },
