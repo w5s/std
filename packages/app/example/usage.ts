03ab821bcf927db0063138da509a4ae36f07f7c8
Julien Polo <julien.polo@gmail.com>
2023-02-14 22:27:03 +0100

    📝 Update usage

--- packages/app/example/usage.ts
+++ packages/app/example/usage.ts
@@ -1,7 +1,9 @@
-import { useRef } from '@w5s/app';
+import { globalStorage, useRef } from '@w5s/app';
 
-export function main() {
+export function main(): void {
   const counterRef = useRef('counter', 1);
+
   counterRef.current += 1;
   console.log(counterRef.current); // 2
+  console.log(globalStorage.get('counter')); // 2
 }
