79717775a201a4a04343c049b6deaa858ce0f2e3
Julien Polo <julien.polo@gmail.com>
2023-08-09 21:55:24 +0200

    ♻️ Move assign and extend to separate files

--- packages/core/src/prelude.spec.ts
+++ packages/core/src/prelude.spec.ts
@@ -1,6 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { assertType } from './type.js';
-import { assign, extend, identity, constant } from './prelude.js';
+import { identity, constant } from './prelude.js';
 
 describe('identity', () => {
   it.each([1, undefined, {}])('should return the same unchanged value', (value) => {
@@ -10,46 +9,5 @@ describe('identity', () => {
 describe('constant', () => {
   it.each([1, undefined, {}])('should return the same unchanged value', (value) => {
     expect(constant(value)('abc')).toBe(value);
-  });
-});
-describe('assign', () => {
-  it('should return identity when null or undefined or empty object is passed', () => {
-    const anyObject = { foo: true };
-    expect(assign(anyObject, undefined)).toBe(anyObject);
-    expect(assign(anyObject, null)).toBe(anyObject);
-    expect(assign(anyObject, {})).toBe(anyObject);
-  });
-  it('should return a new object when another object is passed', () => {
-    expect(assign({ unchanged: '', foo: true }, { foo: false })).toEqual({ unchanged: '', foo: false });
-  });
-  it('should return identity when values are shallow equals', () => {
-    const object = { unchanged: '', foo: true };
-    expect(assign(object, { foo: true })).toBe(object);
-  });
-  it('should not allow adding property', () => {
-    const object = { unchanged: '', foo: true };
-    // @ts-expect-error notAllowed is not present in object
-    assign(object, { notAllowed: false });
-  });
-});
-describe('extend', () => {
-  it('should return identity when null or undefined is passed', () => {
-    const anyObject = { foo: true };
-    expect(extend(anyObject, undefined)).toBe(anyObject);
-    expect(extend(anyObject, null)).toBe(anyObject);
-    expect(extend(anyObject, {})).toBe(anyObject);
-  });
-  it('should return identity when values are shallow equals', () => {
-    const object = { unchanged: '', foo: true };
-    expect(assign(object, { foo: true })).toBe(object);
-  });
-  it('should return a new object when another object is passed', () => {
-    const result = extend({ unchanged: '', override: true }, { override: 'false', newProperty: false });
-    expect(result).toEqual({
-      unchanged: '',
-      override: 'false',
-      newProperty: false,
-    });
-    assertType<typeof result, { unchanged: string; override: string; newProperty: boolean }>(true);
   });
 });
--- packages/core/src/prelude.ts
+++ packages/core/src/prelude.ts
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
-import type { Spread } from './type.js';
-
 /**
  * Return the unchanged given `value`
  *
@@ -28,80 +23,4 @@ export function identity<T>(value: T): T {
  */
 export function constant<T>(value: T): (anyValue?: unknown) => T {
   return () => value;
-}
-
-/**
- * Type safe and immutable equivalent of `{ ...source, ...properties }`.
- * The return type is the same as `source` :
- * - `properties` cannot contain new property
- * - `properties` cannot change the type of a value
- *
- * @example
- * ```typescript
- * const base = { a: 1 }
- * const newObject = assign(base, { a: 2 });// { a: 2 }
- * ```
- * @param source - a base object
- * @param properties - an extension object map
- */
-export function assign<T>(source: T, properties: Partial<T> | undefined | null): T {
-  return mergeObject(source, properties);
-}
-
-/**
- * Type safe and immutable equivalent of `{ ...source, ...extension }`.
- * The return type can change from `source` :
- * - `extension` can add new properties
- * - `extension` can override the type of `source`
- *
- * @example
- * ```typescript
- * const base = { a: 1 }
- * const newObject = assign(base, { b: 3 });// { a: 2, b: 3 }
- * ```
- * @param source - a base object
- * @param extension - an extension object map
- */
-export function extend<T, Ext>(source: T, extension: Ext): Spread<T, Ext> {
-  return mergeObject(source, extension);
-}
-
-function cloneObject<V>(source: V): V {
-  return { ...source };
-}
-
-function mergeObject(source: any, properties: any): any {
-  /* eslint-disable  @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
-
-  if (properties == null) {
-    return source;
-  }
-  const keys = Object.keys(properties);
-  const keyCount = keys.length;
-  if (keyCount === 0) {
-    return source;
-  }
-  const returnValue = cloneObject(source);
-  let changed = false;
-  let keyIndex = 0;
-  // Try to copy until the first changed value is found
-  while (keyIndex < keyCount) {
-    const key = keys[keyIndex]!;
-    const value = properties[key];
-    if (returnValue[key] !== value) {
-      changed = true;
-      returnValue[key] = value;
-      break;
-    }
-    keyIndex += 1;
-  }
-  // Fast Copy every other keys
-  while (keyIndex < keyCount) {
-    const key = keys[keyIndex]!;
-    returnValue[key] = properties[key];
-    keyIndex += 1;
-  }
-
-  return changed ? returnValue : source;
-  /* eslint-enable  @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
 }
