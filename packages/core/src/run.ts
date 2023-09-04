39cd8b4af817214ad5f896988035836d7d094d4c
Julien Polo <julien.polo@gmail.com>
2023-09-04 21:40:58 +0200

    ♻️ Refactor inline helpers

--- packages/core/src/run.ts
+++ packages/core/src/run.ts
@@ -4,6 +4,19 @@ import type { Result } from './result.js';
 import type { Task } from './task.js';
 import type { Awaitable } from './type.js';
 
+// Inline utilities
+const isObject = (anyValue: unknown): anyValue is Record<string, unknown> =>
+  typeof anyValue === 'object' && anyValue !== null;
+const isPromise = <V>(anyValue: unknown): anyValue is Promise<V> =>
+  isObject(anyValue) && typeof anyValue['then'] === 'function' && typeof anyValue['catch'] === 'function';
+const returnOrThrow = <V, E>(result: Result<V, E>): V => {
+  if (result.ok) {
+    return result.value;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+  throw result.error;
+};
+
 /**
  * Interface used to cancel running task
  */
@@ -62,7 +75,7 @@ export const Canceler = {
  */
 export function unsafeRun<Value, Error>(
   task: Task<Value, Error>,
-  cancelerRef: Canceler = { current: undefined }
+  canceler: Canceler = { current: undefined }
 ): Awaitable<Result<Value, Error>> {
   let returnValue: Result<Value, Error> | undefined;
   let resolveHandler = (result: Result<Value, Error>) => {
@@ -72,7 +85,7 @@ export function unsafeRun<Value, Error>(
   const runValue: void | Promise<void> = task.taskRun({
     resolve: (value) => resolveHandler({ _: 'Ok', ok: true, value }),
     reject: (error) => resolveHandler({ _: 'Error', ok: false, error }),
-    canceler: cancelerRef,
+    canceler,
     run: unsafeRun,
   });
   // Try to catch promise errors
@@ -106,20 +119,5 @@ export function unsafeRunOk<Value>(task: Task<Value, unknown>, cancelerRef?: Can
   const promiseOrValue = unsafeRun(task, cancelerRef);
   // @ts-ignore - we assume PromiseLike.then returns a Promise
   // eslint-disable-next-line promise/prefer-await-to-then
-  return isPromise(promiseOrValue) ? promiseOrValue.then(unsafeResultValue) : unsafeResultValue(promiseOrValue);
-}
-
-function unsafeResultValue<V, E>(result: Result<V, E>) {
-  if (result._ === 'Ok') {
-    return result.value;
-  }
-  // eslint-disable-next-line @typescript-eslint/no-throw-literal
-  throw result.error;
-}
-
-function isPromise<V>(anyValue: unknown): anyValue is Promise<V> {
-  return isObject(anyValue) && typeof anyValue['then'] === 'function' && typeof anyValue['catch'] === 'function';
-}
-function isObject(anyValue: unknown): anyValue is Record<string, unknown> {
-  return typeof anyValue === 'object' && anyValue !== null;
+  return isPromise(promiseOrValue) ? promiseOrValue.then(returnOrThrow) : returnOrThrow(promiseOrValue);
 }
