29fa2d6e1d184e9dd899d7a7bd639d7dae6da80b
Julien Polo <julien.polo@gmail.com>
2023-09-19 21:39:38 +0200

    ♻️ Rename fsStub

--- packages/core/src/testing/taskStub.spec.ts
+++ packages/core/src/testing/taskStub.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { taskStub } from './task.js';
+import { taskStub } from './taskStub.js';
 import { unsafeRun } from '../run.js';
 import { Result } from '../result.js';
 
