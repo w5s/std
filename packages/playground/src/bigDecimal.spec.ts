901b49b16d38404fc59e645fdfa88f1d02dc8b86
Julien Polo <julien.polo@gmail.com>
2024-01-13 23:12:00 +0100

    🚧 Add BigDecimal experiment

---
+++ packages/playground/src/bigDecimal.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import { BigDecimal } from './bigDecimal.js';
+
+describe('BigDecimal', () => {
+  describe('()', () => {
+    it('constructs from parameters', () => {
+      expect(BigDecimal(1n, 100)).toEqual({
+        _: 'BigDecimal',
+        value: 1n,
+        scale: 100,
+      });
+    });
+    it('constructs from string', () => {
+      expect(BigDecimal('2')).toEqual(BigDecimal(2n, 0));
+      expect(BigDecimal('-2')).toEqual(BigDecimal(-2n, 0));
+      expect(BigDecimal('0.123')).toEqual(BigDecimal(123n, 3));
+      expect(BigDecimal('200')).toEqual(BigDecimal(200n, 0));
+      expect(BigDecimal('20000000')).toEqual(BigDecimal(20_000_000n, 0));
+      expect(BigDecimal('-20000000')).toEqual(BigDecimal(-20_000_000n, 0));
+      expect(BigDecimal('2.00')).toEqual(BigDecimal(200n, 2));
+      expect(BigDecimal('0.0000200')).toEqual(BigDecimal(200n, 7));
+      // expect(BigDecimal('')).toEqual(BigDecimal.normalize(BigDecimal(0n, 0)));
+      expect(() => BigDecimal('1E5')).toThrow(new Error('1E5 is not a valid BigDecimal'));
+    });
+  });
+});
---
+++ packages/playground/src/bigDecimal.ts
@@ -0,0 +1,61 @@
+import type { Option } from '@w5s/core';
+import { DataObject } from '@w5s/core/dist/dataObject.js';
+import { invariant } from '@w5s/invariant';
+
+const create = (value: bigint, scale: number): BigDecimal => ({ _: 'BigDecimal', value, scale });
+const unsafeParse = (value: string): Option<BigDecimal> => {
+  let digits: string;
+  let scale: number;
+
+  const dot = value.search(/\./);
+  if (dot === -1) {
+    digits = value;
+    scale = 0;
+  } else {
+    const lead = value.slice(0, dot);
+    const trail = value.slice(dot + 1);
+    digits = `${lead}${trail}`;
+    scale = trail.length;
+  }
+
+  if (digits === '') {
+    // TODO: This mimics the BigInt constructor behavior. Should this be `Option.none()`?
+    // return zero;
+    return undefined;
+  }
+
+  if (!/^[+-]?\d+$/.test(digits)) {
+    return undefined;
+  }
+
+  return create(BigInt(digits), scale);
+};
+
+export interface BigDecimal
+  extends DataObject<{
+    _: 'BigDecimal';
+    value: bigint;
+    scale: number;
+  }> {}
+
+/**
+ * @namespace
+ */
+export const BigDecimal = Object.assign(
+  DataObject.MakeGeneric(
+    'BigDecimal',
+    (
+      _
+    ): {
+      (stringValue: string): BigDecimal;
+      (value: bigint, scale: number): BigDecimal;
+    } =>
+      (value: string | bigint, scale?: number): BigDecimal =>
+        typeof value === 'string'
+          ? unsafeParse(value) ?? invariant(false, `${String(value)} is not a valid BigDecimal`)
+          : create(value, scale as number)
+  ),
+  {
+    // ...BigDecimalNumeric,
+  }
+);
