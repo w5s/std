bf14de12d45fd8211541cd484785cb91a774d64e
Julien Polo <julien.polo@gmail.com>
2024-01-16 14:23:05 +0100

    ♻️ Refactor currency as more sparse aggregate

--- packages/money/src/currency.ts
+++ packages/money/src/currency.ts
@@ -2,9 +2,38 @@ import type { Int } from '@w5s/core';
 import { Struct } from '@w5s/core/dist/struct.js';
 import { Comparable } from '@w5s/core/dist/comparable.js';
 
+/**
+ * Default rounding when omitted
+ */
+const defaultRounding = 0 as Int;
+/**
+ * Default precision when omitted
+ */
+const defaultPrecision = 2 as Int;
+
 const CurrencyComparable = Comparable<Currency>({
   compare: (left, right) => (left.code === right.code ? 0 : left.code < right.code ? -1 : 1),
 });
+const CurrencyStruct = Struct.MakeGeneric(
+  'Currency',
+  (_) =>
+    (parameters: {
+      name: Currency['name'];
+      namePlural?: Currency['namePlural'];
+      code: Currency['code'];
+      precision?: Currency['precision'];
+      rounding?: Currency['rounding'];
+      symbol: Currency['symbol'];
+      symbolNative?: Currency['symbolNative'];
+    }): Currency => ({
+      _,
+      rounding: defaultRounding,
+      precision: defaultPrecision,
+      namePlural: parameters.name,
+      symbolNative: parameters.symbol,
+      ...parameters,
+    })
+);
 
 export interface Currency
   extends Struct<{
@@ -44,37 +73,15 @@ export interface Currency
      */
     symbolNative: string;
   }> {}
-export const Currency = Object.assign(
-  Struct.MakeGeneric(
-    'Currency',
-    (_) =>
-      (parameters: {
-        name: Currency['name'];
-        namePlural?: Currency['namePlural'];
-        code: Currency['code'];
-        precision?: Currency['precision'];
-        rounding?: Currency['rounding'];
-        symbol: Currency['symbol'];
-        symbolNative?: Currency['symbolNative'];
-      }): Currency => ({
-        _,
-        rounding: Currency.defaultRounding,
-        precision: Currency.defaultPrecision,
-        namePlural: parameters.name,
-        symbolNative: parameters.symbol,
-        ...parameters,
-      })
-  ),
-  {
-    /**
-     * Default rounding when omitted
-     */
-    defaultRounding: 0 as Int,
-    /**
-     * Default precision when omitted
-     */
-    defaultPrecision: 2 as Int,
+export const Currency = Object.assign(CurrencyStruct, {
+  /**
+   * Default rounding when omitted
+   */
+  defaultRounding,
+  /**
+   * Default precision when omitted
+   */
+  defaultPrecision,
 
-    ...CurrencyComparable,
-  }
-);
+  ...CurrencyComparable,
+});
