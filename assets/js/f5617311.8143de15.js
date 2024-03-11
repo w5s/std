64c958c034d89d96f503e71173f2d9e4d6282549
jpolo <jpolo@users.noreply.github.com>
2024-03-11 21:12:03 +0000

    deploy: 0b7f2b63314e1ca0b6a9746a21ffb278433d929d

---
+++ assets/js/f5617311.8143de15.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_w5s_website=self.webpackChunk_w5s_website||[]).push([[4755],{261:s=>{s.exports=JSON.parse('{"title":"Basics","slug":"/category/basics","permalink":"/std/docs/category/basics","navigation":{"previous":{"title":"Manual","permalink":"/std/docs/category/manual"},"next":{"title":"Enum","permalink":"/std/docs/manual/basics/enum"}}}')}}]);
