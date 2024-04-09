cbf2dcc2206b68f00e364a815dfa40a8d7a09942
Julien Polo <julien.polo@gmail.com>
2024-04-09 23:38:20 +0200

    📝 Fix site links

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -55,7 +55,7 @@ const config: Config = (() => {
 
     // Set the production url of your site here
     // url: 'https://your-docusaurus-test-site.com',
-    url: `https://${organizationName}.github.com/`,
+    url: `https://${organizationName}.github.io/`,
 
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
