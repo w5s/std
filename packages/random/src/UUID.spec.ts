3e4b996b296530eea2ff3f23d04e8f6921f4eade
Julien Polo <julien.polo@gmail.com>
2024-04-17 23:23:32 +0200

    ♻️ Use Tag.define for UUID

--- packages/random/src/UUID.spec.ts
+++ packages/random/src/UUID.spec.ts
@@ -1,11 +1,36 @@
 import { describe, it, expect } from 'vitest';
-import { Codec } from '@w5s/core/dist/Codec.js';
+import { DecodeError } from '@w5s/core/dist/Codec.js';
 import { Result } from '@w5s/core';
+import { describeCodec, describeType } from '@w5s/core/dist/testing.js';
 import { UUID } from './UUID.js';
 
 describe('UUID', () => {
   const anyValidUUID = '1c19548b-7cac-4222-b722-dc38f2870669';
 
+  describeType({ describe, it, expect })(UUID, {
+    typeName: 'UUID',
+    instances: () => [anyValidUUID],
+    notInstances: () => [null, anyValidUUID.slice(1)],
+  });
+  describeCodec({ describe, it, expect })(UUID, {
+    decode: [
+      [anyValidUUID, Result.Ok(anyValidUUID)],
+      [
+        null,
+        Result.Error(
+          DecodeError({
+            message: 'Invalid UUID',
+            input: null,
+          })
+        ),
+      ],
+    ],
+    encode: [[anyValidUUID, anyValidUUID]],
+    schema: () => ({
+      type: 'string',
+      format: 'uuid',
+    }),
+  });
   describe('of()', () => {
     it('should return a new representation', () => {
       const uuid = UUID.of(anyValidUUID);
@@ -21,33 +46,6 @@ describe('UUID', () => {
   describe('.empty', () => {
     it('should return a 0 based UUID', () => {
       expect(UUID.empty()).toBe('00000000-0000-0000-0000-000000000000');
-    });
-  });
-  describe('.hasInstance', () => {
-    it.each([undefined, null, [], '', 'non uuid'])(`should return false for invalid UUID`, (value) => {
-      expect(UUID.hasInstance(value)).toBe(false);
-    });
-    it(`should return true for valid UUID`, () => {
-      expect(UUID.hasInstance(anyValidUUID)).toBe(true);
-    });
-  });
-  describe('codecEncode', () => {
-    it('should encode value', () => {
-      expect(Codec.encode(UUID, UUID.of(anyValidUUID))).toEqual(anyValidUUID);
-    });
-  });
-  describe('codecDecode', () => {
-    it('should encode value', () => {
-      const decoded = Codec.decode(UUID, anyValidUUID);
-      expect(decoded).toEqual(Result.Ok(anyValidUUID));
-    });
-  });
-  describe('codecSchema', () => {
-    it('should return value', () => {
-      expect(Codec.schema(UUID)).toEqual({
-        type: 'string',
-        format: 'uuid',
-      });
     });
   });
 });
--- packages/random/src/UUID.ts
+++ packages/random/src/UUID.ts
@@ -1,31 +1,19 @@
-import { invariant } from '@w5s/invariant';
-import { DecodeError, Codec } from '@w5s/core/dist/Codec.js';
-import type { Tag } from '@w5s/core';
+import { Tag } from '@w5s/core/dist/Tag.js';
 
 /**
  * UUID string type
  */
-export type UUID = string & Tag<'UUID'>;
+export type UUID = `${string}-${string}-${string}-${string}` & Tag<'UUID'>;
 
 const uuidRegexp = /^[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[89ab][\da-f]{3}-[\da-f]{12}$/i;
-const isUUID = (anyValue: unknown): anyValue is UUID => typeof anyValue === 'string' && uuidRegexp.test(anyValue);
-const UUIDCodec = Codec<UUID>({
-  codecEncode: String,
-  codecDecode(value) {
-    return isUUID(value)
-      ? { _: 'Ok', ok: true, value }
-      : {
-          _: 'Error',
-          ok: false,
-          error: DecodeError({ message: `${String(value)} is not a valid UUID`, input: value }),
-        };
-  },
-  codecSchema() {
-    return {
-      type: 'string',
-      format: 'uuid',
-    };
-  },
+
+const UUIDType = Tag.define<`${string}-${string}-${string}-${string}`, UUID>({
+  typeName: 'UUID',
+  hasInstance: (anyValue) => typeof anyValue === 'string' && uuidRegexp.test(anyValue),
+  codecSchema: () => ({
+    type: 'string',
+    format: 'uuid',
+  }),
 });
 
 /**
@@ -33,9 +21,7 @@ const UUIDCodec = Codec<UUID>({
  *
  * @namespace
  */
-export const UUID = {
-  ...UUIDCodec,
-
+export const UUID = Object.assign(UUIDType, {
   /**
    * Returns an `UUID` with only `0`
    *
@@ -60,23 +46,6 @@ export const UUID = {
    * @param value - the string representation
    */
   of(value: `${string}-${string}-${string}-${string}`): UUID {
-    invariant(isUUID(value), `${value} is not a valid UUID`);
-    return value;
+    return UUIDType.wrap(value);
   },
-
-  /**
-   * Returns `true` if `anyValue` is a valid `UUID`
-   *
-   * @example
-   * ```typescript
-   * UUID.hasInstance(undefined);// false
-   * UUID.hasInstance('');// false
-   * UUID.hasInstance('c106a26a-21bb-5538-8bf2-57095d1976c1');// true
-   * ```
-   * @category Type
-   * @param anyValue - the value to tested
-   */
-  hasInstance(anyValue: unknown): anyValue is UUID {
-    return isUUID(anyValue);
-  },
-};
+});
