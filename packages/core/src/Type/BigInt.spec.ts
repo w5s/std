05b1925c4c1604f14c1d58fab7c4c68084fafc85
Julien Polo <julien.polo@gmail.com>
2024-05-23 14:00:16 +0200

    ✨ Add BigInt Codec

--- packages/core/src/Type/BigInt.spec.ts
+++ packages/core/src/Type/BigInt.spec.ts
@@ -1,11 +1,28 @@
 import { describe, it, expect } from 'vitest';
 import { BigInt } from './BigInt.js';
-import { describeType } from '../testing.js';
+import { describeCodec, describeType } from '../testing.js';
+import { Result } from '../Result.js';
+import { DecodeError } from '../Codec.js';
 
 describe('BigInt', () => {
   describeType({ describe, it, expect })(BigInt, {
     typeName: 'BigInt',
     instances: () => [1n, 0n],
     notInstances: () => ['anything', null, undefined, BigInt.hasInstance],
+  });
+  describeCodec({ describe, it, expect })(BigInt, {
+    encode: [
+      [0n, '0n'],
+      [1n, '1n'],
+    ],
+    decode: [
+      ['1n', Result.Ok(1n)],
+      ['-2n', Result.Ok(-2n)],
+      ['2', Result.Error(DecodeError({ message: 'Cannot decode 2 as BigInt', input: '2' }))],
+      ['2.1n', Result.Error(DecodeError({ message: 'Cannot decode 2.1n as BigInt', input: '2.1n' }))],
+      [undefined, Result.Error(DecodeError({ message: 'Cannot decode undefined as BigInt', input: undefined }))],
+      [null, Result.Error(DecodeError({ message: 'Cannot decode null as BigInt', input: null }))],
+    ],
+    schema: () => ({ type: 'string', format: 'bigint' }),
   });
 });
--- packages/core/src/Type/BigInt.ts
+++ packages/core/src/Type/BigInt.ts
@@ -10,4 +10,16 @@ export const BigInt = define<bigint>({
   hasInstance(anyValue: unknown): anyValue is bigint {
     return typeof anyValue === 'bigint';
   },
+  codecEncode: (input) => `${input.toString(10)}n`,
+  codecDecode: (input, { ok, error }) => {
+    if (typeof input === 'string' && input.endsWith('n')) {
+      try {
+        return ok(globalThis.BigInt(input.slice(0, -1)));
+      } catch {
+        // do nothing, let it return an error
+      }
+    }
+    return error(input, 'BigInt');
+  },
+  codecSchema: () => ({ type: 'string', format: 'bigint' }),
 });
