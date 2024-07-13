57d6cfe45f4070e977b2ad568785105f0fbe4fc5
Julien Polo <julien.polo@gmail.com>
2024-07-13 22:25:06 +0200

    ⚗️ Add Duration

---
+++ packages/playground/src/Duration.spec.ts
@@ -0,0 +1,60 @@
+import { describeType, describeCodec } from '@w5s/core/dist/Testing.js';
+import { describe, it, expect } from 'vitest';
+import { DecodeError, Int, Result } from '@w5s/core';
+import { Duration } from './Duration.js';
+
+describe('Duration', () => {
+  describeType({ describe, it, expect })(Duration, {
+    typeName: 'Duration',
+    instances: () => [
+      Duration({
+        years: Int(1),
+        months: Int(2),
+        weeks: Int(3),
+        days: Int(4),
+        hours: Int(5),
+        minutes: Int(6),
+        seconds: 7,
+      }),
+    ],
+    notInstances: () => ['1', 1.1, undefined, {}],
+  });
+  describeCodec({ describe, it, expect })(Duration, {
+    decode: [
+      [
+        'P2Y4M3W6DT14H30M20.42S',
+        Result.Ok(
+          Duration({
+            years: Int(2),
+            months: Int(4),
+            weeks: Int(3),
+            days: Int(6),
+            hours: Int(14),
+            minutes: Int(30),
+            seconds: 20.42,
+          })
+        ),
+      ],
+      // Invalid inputs
+      ...['', 'P', 'PT'].map(
+        (_) =>
+          [_, Result.Error(DecodeError({ message: `Cannot decode ${_} as Duration`, input: _ }))] as [
+            string,
+            Result<Duration, DecodeError>,
+          ]
+      ),
+    ],
+    encode: [
+      [
+        Duration({ years: Int(1), months: Int(2), days: Int(3), hours: Int(4), minutes: Int(5), seconds: 6 }),
+        'P1Y2M3DT4H5M6S',
+      ],
+      [Duration({ years: Int(1), months: Int(2), days: Int(3) }), 'P1Y2M3D'],
+      [Duration({ hours: Int(4), minutes: Int(5), seconds: 6.2 }), 'PT4H5M6.2S'],
+    ],
+    schema: () => ({
+      type: 'string',
+      format: 'duration',
+    }),
+  });
+});
---
+++ packages/playground/src/Duration.ts
@@ -0,0 +1,117 @@
+import type { Codec, Int, Option } from '@w5s/core';
+import { Struct } from '@w5s/core/dist/Struct.js';
+import { parse as parseInt } from '@w5s/core/dist/Int/parse.js';
+import { parse as parseNumber } from '@w5s/core/dist/Number/parse.js';
+
+const numbers = '\\d+';
+const fractionalNumbers = `${numbers}(?:[\\.,]${numbers})?`;
+const date = `(${numbers}Y)?(${numbers}M)?(${numbers}W)?(${numbers}D)?`;
+const time = `T(${fractionalNumbers}H)?(${fractionalNumbers}M)?(${fractionalNumbers}S)?`;
+const durationISO8601 = `P(?:${date}(?:${time})?)`;
+
+export const durationRegexp = new RegExp(durationISO8601);
+
+function parse(expression: string): Option<Duration> {
+  const matches = expression.replaceAll(',', '.').match(durationRegexp);
+  if (matches == null) {
+    return undefined;
+  }
+  // Slice away first entry in match-array (the input string)
+  const slicedMatches: Array<string | undefined> = matches.slice(1);
+  if (slicedMatches.every((v) => v == null)) {
+    return undefined;
+  }
+  const [
+    yearsParsed = '0',
+    monthsParsed = '0',
+    weeksParsed = '0',
+    daysParsed = '0',
+    hoursParsed = '0',
+    minutesParsed = '0',
+    secondsParsed = '0',
+  ] = slicedMatches;
+  const years = parseInt(yearsParsed);
+  const months = parseInt(monthsParsed);
+  const weeks = parseInt(weeksParsed);
+  const days = parseInt(daysParsed);
+  const hours = parseInt(hoursParsed);
+  const minutes = parseInt(minutesParsed);
+  const seconds = parseNumber(secondsParsed);
+
+  if (
+    years === undefined ||
+    months === undefined ||
+    weeks === undefined ||
+    days === undefined ||
+    hours === undefined ||
+    minutes === undefined ||
+    seconds === undefined
+  ) {
+    return undefined;
+  }
+
+  return {
+    _: 'Duration',
+    years,
+    months,
+    weeks,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+const formatPart = (prefix: string, part: number) => (part === 0 ? '' : `${part}${prefix}`);
+const formatSection = (prefix: string, content: string) => (content.length === 0 ? '' : `${prefix}${content}`);
+
+function format(duration: Duration): string {
+  return `P${formatPart('Y', duration.years)}${formatPart('M', duration.months)}${formatPart('D', duration.days)}${formatSection(
+    'T',
+    `${formatPart('H', duration.hours)}${formatPart('M', duration.minutes)}${formatPart('S', duration.seconds)}`
+  )}`;
+}
+
+export interface Duration
+  extends Struct<{
+    _: 'Duration';
+    years: Int;
+    months: Int;
+    weeks: Int;
+    days: Int;
+    hours: Int;
+    minutes: Int;
+    seconds: number;
+  }> {}
+
+const DurationCodec: Codec<Duration> = {
+  codecEncode: (value) => format(value),
+  codecDecode: (input, { ok, error }) => {
+    const returnValue = typeof input === 'string' ? parse(input) : undefined;
+    return returnValue === undefined ? error(input, 'Duration') : ok(returnValue);
+  },
+  codecSchema: () => ({
+    type: 'string',
+    format: 'duration',
+  }),
+};
+
+/**
+ * @namespace
+ */
+export const Duration = Object.assign(
+  (properties?: Partial<Struct.Parameters<Duration>>): Duration => ({
+    _: 'Duration',
+    years: properties?.years ?? (0 as Int),
+    months: properties?.months ?? (0 as Int),
+    weeks: properties?.weeks ?? (0 as Int),
+    days: properties?.days ?? (0 as Int),
+    hours: properties?.hours ?? (0 as Int),
+    minutes: properties?.minutes ?? (0 as Int),
+    seconds: properties?.seconds ?? 0,
+  }),
+  {
+    ...Struct.define<Duration>('Duration'),
+    ...DurationCodec,
+  }
+);
