cd1fb5b30b1d7c70729ee1bee9779f5d7c124d55
Julien Polo <julien.polo@gmail.com>
2024-08-28 23:56:59 +0200

    ✨ Add Primitive type

---
+++ packages/core-type/src/Primitive.test-d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Primitive } from './Primitive.js';
+
+const num: Primitive = 1;
+const str: Primitive = '';
+const boolean: Primitive = true;
+const symbol: Primitive = Symbol('');
+const undef: Primitive = undefined;
+const nul: Primitive = null;
+const bigint: Primitive = 1n;
--- packages/core-type/src/index.ts
+++ packages/core-type/src/index.ts
@@ -4,3 +4,4 @@ export * from './assertType.js';
 export * from './Nullable.js';
 export * from './PartialKeys.js';
 export * from './Pretty.js';
+export * from './Primitive.js';
