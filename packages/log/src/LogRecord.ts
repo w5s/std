e44fac8dba7272d632d1b2582521f26f6187cdd8
Julien Polo <julien.polo@gmail.com>
2024-09-05 23:45:16 +0200

    💚 Fix import

--- packages/log/src/LogRecord.ts
+++ packages/log/src/LogRecord.ts
@@ -1,6 +1,6 @@
 import type { Type, Struct } from '@w5s/core';
 import { Time } from '@w5s/time/dist/Time/Time.js';
-import { UUID } from '@w5s/random/dist/UUID.js';
+import { UUID } from '@w5s/core/dist/Type/UUID.js';
 import { $Object as TObject } from '@w5s/core/dist/Type/Object.js';
 import { constant } from '@w5s/core/dist/Type/constant.js';
 import { string } from '@w5s/core/dist/Type/string.js';
