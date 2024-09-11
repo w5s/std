7cd824df3ad40229fd9973290750851cee9d30af
Julien Polo <julien.polo@gmail.com>
2024-09-11 22:28:02 +0200

    ♻️ Move Request to separate file

--- packages/http/src/HTTP.ts
+++ packages/http/src/HTTP.ts
@@ -1,19 +1,11 @@
 import { invariant } from '@w5s/invariant';
-import type { Option } from '@w5s/core';
 import type { Task } from '@w5s/task';
 import { from } from '@w5s/task/dist/Task/from.js';
 import { andThen } from '@w5s/task/dist/Task/andThen.js';
 import { HTTPError } from './HTTPError.js';
 import type { HTTPParser } from './HTTPParser.js';
-import type { Method } from './Method.js';
-import type { RequestHeaders } from './RequestHeaders.js';
 import type { Response } from './Response.js';
-import type { RequestCredentials } from './RequestCredentials.js';
-import type { RequestCache } from './RequestCache.js';
-import type { RequestRedirect } from './RequestRedirect.js';
-import type { ReferrerPolicy } from './ReferrerPolicy.js';
-import type { RequestMode } from './RequestMode.js';
-import type { RequestDestination } from './RequestDestination.js';
+import type { Request as RequestDefault } from './Request.js';
 
 /**
  * Types
@@ -21,94 +13,6 @@ import type { RequestDestination } from './RequestDestination.js';
 type NativeFetch = typeof globalThis.fetch;
 
 export namespace HTTP {
-  /**
-   * HTTP URL type
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/API/Request/url
-   */
-  export type URL = string;
-
-  /**
-   * HTTP request type
-   *
-   * @see https://developer.mozilla.org/en-US/docs/Web/API/Request
-   */
-  export interface Request {
-    // https://fetch.spec.whatwg.org/#requests
-    /**
-     * Indicates how the request will interact with the browser's cache to set request's cache.
-     *
-     * @example
-     * ```ts
-     * 'no-store'
-     * ```
-     */
-    readonly cache?: RequestCache;
-    /**
-     * A string indicating whether credentials will be sent with the request always, never, or only when sent to a same-origin URL.
-     */
-    readonly credentials?: RequestCredentials;
-    readonly destination?: RequestDestination;
-    /**
-     * Request URL
-     *
-     * @example
-     * 'https://foo.com'
-     */
-    readonly url: HTTP.URL;
-    /**
-     * Request headers
-     *
-     * @example
-     * ```ts
-     * { "Content-type": "application/json" }
-     * ```
-     */
-    readonly headers?: RequestHeaders;
-    /**
-     * A cryptographic hash of the resource to be fetched by request. Sets request's integrity.
-     */
-    readonly integrity?: string;
-    /**
-     * Indicates whether request follows redirects, results in an error upon encountering a redirect, or returns the redirect (in an opaque fashion).
-     */
-    readonly redirect?: RequestRedirect;
-    /**
-     * Request referrer policy
-     */
-    readonly referrerPolicy?: ReferrerPolicy;
-    /**
-     * Request Method
-     *
-     * @example 'GET', 'POST'
-     */
-    readonly method?: Method;
-    /**
-     * A boolean to set request's keepalive.
-     */
-    readonly keepalive?: boolean;
-    /**
-     * Indicates whether the request will use CORS, or will be restricted to same-origin URLs. Sets request's mode.
-     */
-    readonly mode?: RequestMode;
-    /**
-     * An optional BodyInit object to set request's body.
-     */
-    readonly body?: Option<BodyInit>;
-    /**
-     * A string whose value is a same-origin URL, "about:client", or the empty string, to set request's referrer.
-     */
-    readonly referrer?: string;
-    /**
-     * An AbortSignal to set request's signal.
-     */
-    // readonly signal?: Option<AbortSignal>;
-    /**
-     * Can only be undefined. Used to disassociate request from any Window.
-     */
-    readonly window?: undefined;
-  }
-
   /**
    * Return a new {@link @w5s/core!Task} that will send an HTTP request
    *
@@ -127,8 +31,7 @@ export namespace HTTP {
     return andThen(applyFetch(localFetch, fetchRequest), parse);
   }
   export namespace request {
-    // eslint-disable-next-line @typescript-eslint/no-shadow
-    export interface Request<Value> extends HTTP.Request {
+    export interface Request<Value> extends RequestDefault {
       /**
        * Response Parser
        */
@@ -159,7 +62,7 @@ function getDefaultFetch() {
 
 function applyFetch(
   fetchFn: NativeFetch,
-  request: HTTP.Request
+  request: RequestDefault
 ): Task<Response, HTTPError.InvalidURL | HTTPError.NetworkError> {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return from(async ({ resolve, reject, canceler }) => {
