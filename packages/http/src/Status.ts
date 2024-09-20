3515f890d470cd894da10c17c005e0b3a1884cd2
Julien Polo <julien.polo@gmail.com>
2024-09-20 23:54:27 +0200

    ♻️ Add Callable to Status module

--- packages/http/src/Status.ts
+++ packages/http/src/Status.ts
@@ -1,3 +1,4 @@
+import { Callable } from '@w5s/core/dist/Callable.js';
 import { Status as StatusType } from './Status/Status.js';
 import { StatusBounded } from './Status/StatusBounded.js';
 import { StatusComparable } from './Status/StatusComparable.js';
@@ -6,11 +7,11 @@ import * as StatusAll from './Status/status.all.js';
 /**
  * @namespace
  */
-export const Status = {
+export const Status = Callable({
   ...StatusType,
   ...StatusAll,
   ...StatusBounded,
   ...StatusComparable,
-};
+});
 
 export interface Status extends StatusType {}
