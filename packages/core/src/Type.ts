ca1a26d283b36fb2703c0c462892c1201e321289
Julien Polo <julien.polo@gmail.com>
2024-09-22 22:43:29 +0200

    🏷️ Improve typing

--- packages/core/src/Type.ts
+++ packages/core/src/Type.ts
@@ -51,7 +51,7 @@ export interface Type<T> {
    * @category Type
    * @param anyValue
    */
-  hasInstance(anyValue: unknown): anyValue is T;
+  hasInstance: (anyValue: unknown) => anyValue is T;
   /**
    * Try to convert anyValue to enum value or else returns `Option.None`
    *
@@ -64,7 +64,7 @@ export interface Type<T> {
    * @category Constructor
    * @param anyValue
    */
-  from(anyValue: unknown): Option<T>;
+  from: (anyValue: unknown) => Option<T>;
   /**
    * When defined, returns a custom string representation
    *
