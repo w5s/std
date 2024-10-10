3c13aaf9c61e0607729a2835b6ea2dd98ba339f5
Julien Polo <julien.polo@gmail.com>
2024-10-10 22:24:14 +0200

    ✅ Fix test typing

--- packages/http/src/Status/StatusComparable.spec.ts
+++ packages/http/src/Status/StatusComparable.spec.ts
@@ -1,14 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import { describeComparable } from '@w5s/core/dist/Testing.js';
+import { Int } from '@w5s/core';
 import { StatusComparable } from './StatusComparable.js';
 import { Status } from './Status.js';
 
 describe('StatusComparable', () => {
   describeComparable({ describe, it, expect })(StatusComparable, {
-    ordered: () => [Status(100, 'z'), Status(101, 'b'), Status(500, 'a')],
+    ordered: () => [Status(Int(100), 'z'), Status(Int(101), 'b'), Status(Int(500), 'a')],
     equivalent: () => [
-      [Status(100, 'z'), Status(100, 'z')],
-      [Status(100, 'z'), Status(100, 'a')],
+      [Status(Int(100), 'z'), Status(Int(100), 'z')],
+      [Status(Int(100), 'z'), Status(Int(100), 'a')],
     ],
   });
 });
