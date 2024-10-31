69d6e0998f03d5dd547525756b80dfc80f1ff638
Julien Polo <julien.polo@gmail.com>
2024-10-31 17:18:02 +0100

    ♻️ Use core-type.TestingLibrary

--- packages/core/src/Testing/type.ts
+++ packages/core/src/Testing/type.ts
@@ -1,17 +1,1 @@
-export interface TestingLibrary {
-  describe: {
-    (description: string, fn: () => void): void;
-    todo: (description: string, fn: () => void) => void;
-  };
-  it: {
-    (description: string, fn: () => void): void;
-    todo: (description: string, fn: () => void) => void;
-    each: <T>(values: ReadonlyArray<T>) => (description: string, fn: (value: T) => void) => void;
-  };
-  expect: <V>(value: V) => {
-    toBeLessThan: (anyValue: any) => void;
-    toBeGreaterThan: (anyValue: any) => void;
-    toBe: (anyValue: unknown) => void;
-    toEqual: (anyValue: unknown) => void;
-  };
-}
+export type { TestingLibrary } from '@w5s/core-type';
