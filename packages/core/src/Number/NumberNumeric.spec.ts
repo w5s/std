85ddbabb7d33d833902736b7ab08d1b1fddc767c
Julien Polo <julien.polo@gmail.com>
2024-11-03 23:29:06 +0100

    ✨ Add Numeric.CheckedDivide

--- packages/core/src/Number/NumberNumeric.spec.ts
+++ packages/core/src/Number/NumberNumeric.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { describeAdd, describeMultiply, describeSubtract } from '../Testing.js';
+import { describeAdd, describeMultiply, describeSubtract, describeCheckedDivide } from '../Testing.js';
 import { NumberComparable } from './NumberComparable.js';
 import { NumberNumeric } from './NumberNumeric.js';
 
@@ -16,5 +16,10 @@ describe('NumberNumeric', () => {
     { call: [1, 1], returns: 1 },
     { call: [2, 3], returns: 6 },
     { call: [3, 2], returns: 6 },
+  ]);
+  describeCheckedDivide({ describe, it, expect })({ ...NumberComparable, ...NumberNumeric }, [
+    { call: [1, 1], returns: 1 },
+    { call: [6, 2], returns: 3 },
+    { call: [3, 0], returns: undefined },
   ]);
 });
--- packages/core/src/Number/NumberNumeric.ts
+++ packages/core/src/Number/NumberNumeric.ts
@@ -1,9 +1,17 @@
 import type { Numeric } from '../Numeric.js';
 
-interface NumberNumeric extends Numeric.Add<number>, Numeric.Multiply<number>, Numeric.Subtract<number> {}
+interface NumberNumeric
+  extends Numeric.Add<number>,
+    Numeric.Multiply<number>,
+    Numeric.Subtract<number>,
+    Numeric.CheckedDivide<number> {}
 
 export const NumberNumeric: NumberNumeric = {
   '+': (left, right) => left + right,
   '-': (left, right) => left - right,
   '*': (left, right) => left * right,
+  '/?': (left, right) => {
+    const result = left / right;
+    return Number.isFinite(result) ? result : undefined;
+  },
 };
