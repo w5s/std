9a8edc71c93778cbedc883e7ad95e213ee5365c4
Julien Polo <julien.polo@gmail.com>
2024-11-06 00:22:59 +0100

    ⚗️ Add abortable

---
+++ packages/playground/src/abortable.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest';
+import { FakeTask, withTask } from '@w5s/task/dist/Testing.js';
+import { abortable, AbortError } from './abortable.js';
+
+describe(abortable, () => {
+  const expectTask = withTask(expect);
+
+  it('aborts a task', async () => {
+    const task = FakeTask({
+      delayMs: 1,
+      value: 'my value',
+    });
+    const abortController = new AbortController();
+    const abortTask = abortable(task, abortController.signal);
+
+    const expectation = expectTask(abortTask).toReject(AbortError());
+    abortController.abort();
+    await expectation;
+  });
+});
---
+++ packages/playground/src/abortable.ts
@@ -0,0 +1,24 @@
+import type { TaskLike, Task } from '@w5s/task';
+import { CustomError } from '@w5s/error/dist/CustomError.js';
+import { from } from '@w5s/task/dist/Task/from.js';
+
+export interface AbortError extends CustomError<{ name: 'AbortError' }> {}
+
+export const AbortError = CustomError.define<AbortError>({
+  errorName: 'AbortError',
+  errorMessage: 'The operation was aborted',
+});
+
+export function abortable<Value, Error>(
+  task: TaskLike<Value, Error>,
+  abortSignal: AbortSignal,
+): Task<Value, Error | AbortError> {
+  return from((parameters) => {
+    const { reject, canceler } = parameters;
+    abortSignal.addEventListener('abort', () => {
+      canceler.current?.();
+      reject(AbortError());
+    });
+    task.taskRun(parameters);
+  });
+}
