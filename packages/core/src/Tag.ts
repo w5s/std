2e64c6fea5ad191d4377db4b4e04bc9e7bce12de
Julien Polo <julien.polo@gmail.com>
2024-11-08 18:43:55 +0100

    ♻️ Use core-type.Tag

--- packages/core/src/Tag.ts
+++ packages/core/src/Tag.ts
@@ -1,28 +1,14 @@
+import type { Tag as CoreTypeTag } from '@w5s/core-type';
 import type { Callable } from './Callable.js';
 import type { Codec } from './Codec.js';
 import { define } from './Tag/define.js';
 import type { Type } from './Type.js';
 
 /**
- * Enhance `Base` by adding tags. Every tag is prefixed by `@@` as a convention to never be used by runtime code
+ * Alias of {@link @w5s/core-type!Tag}
  *
- * @example
- * ```typescript
- * type PositiveNumber = number & Tag<{ positive: true }>;
- * const isPositive = (n: number): n is PositiveNumber => n >= 0;
- * const squareRoot = (n: PositiveNumber): PositiveNumber => Math.sqrt(n) as PositiveNumber;
- * const value = 0;
- * squareRoot(value); // tsc: Error
- * if (isPositive(value)) {
- *   squareRoot(value); // tsc: Passed
- * }
- * ```
  */
-export interface Tag<T extends string | symbol> {
-  readonly '@@tag': {
-    readonly [K in T]: K;
-  };
-}
+export type Tag<T extends string | symbol> = CoreTypeTag<T>;
 
 /**
  * @namespace
