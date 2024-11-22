fa49fb71208ac166e7dcb862dd61c50ad6a28272
Julien Polo <julien.polo@gmail.com>
2024-11-23 00:18:04 +0100

    💄 Add social card

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -110,7 +110,7 @@ const config: Config = (() => {
 
     themeConfig: {
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/w5s_social_card.png',
       navbar: {
         title: 'Standard Library',
         logo: {
