20e5f1c33751cebb6a6d4f13c675637cd465412b
Julien Polo <julien.polo@gmail.com>
2024-12-30 12:30:58 +0100

    ♻️ Improve Symbol polyfill

--- packages/core/src/Symbol.ts
+++ packages/core/src/Symbol.ts
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 
 const GlobalSymbol = globalThis.Symbol;
+const __symbolAlias = <K extends keyof globalThis.SymbolConstructor>(name: K): globalThis.SymbolConstructor[K] =>
+  GlobalSymbol[name] ?? (GlobalSymbol.for(name) as any);
 
 declare global {
-  interface SymbolForMap {
+  interface SymbolFor {
     readonly 'w5s.enumKeys': unique symbol;
     readonly 'w5s.toFunction': unique symbol;
     readonly 'nodejs.util.inspect.custom': unique symbol;
   }
   interface SymbolConstructor {
-    for<K extends keyof SymbolForMap>(key: K): SymbolForMap[K];
+    for<K extends keyof SymbolFor>(key: K): SymbolFor[K];
   }
 }
 
@@ -17,17 +19,17 @@ export interface SymbolConstructor extends Omit<globalThis.SymbolConstructor, 'k
   /**
    * Enum keys
    */
-  readonly enumKeys: SymbolForMap['w5s.enumKeys'];
+  readonly enumKeys: SymbolFor['w5s.enumKeys'];
 
   /**
    * Callable callback
    */
-  readonly toFunction: '__toFunction__'; // TODO: unique symbol;
+  readonly toFunction: '__toFunction__'; // TODO: SymbolFor['w5s.toFunction'];
 
   /**
    * NodeJS inspect symbol
    */
-  readonly nodeInspect: SymbolForMap['nodejs.util.inspect.custom'];
+  readonly nodeInspect: SymbolFor['nodejs.util.inspect.custom'];
 }
 
 /**
@@ -37,22 +39,22 @@ export interface SymbolConstructor extends Omit<globalThis.SymbolConstructor, 'k
  */
 export const Symbol: SymbolConstructor = {
   for: GlobalSymbol.for,
-  iterator: GlobalSymbol.iterator,
-  asyncIterator: GlobalSymbol.asyncIterator,
-  hasInstance: GlobalSymbol.hasInstance,
-  isConcatSpreadable: GlobalSymbol.isConcatSpreadable,
-  match: GlobalSymbol.match,
-  replace: GlobalSymbol.replace,
-  search: GlobalSymbol.search,
-  species: GlobalSymbol.species,
-  split: GlobalSymbol.split,
-  toStringTag: GlobalSymbol.toStringTag,
-  toPrimitive: GlobalSymbol.toPrimitive,
-  unscopables: GlobalSymbol.unscopables,
-  matchAll: GlobalSymbol.matchAll,
-  dispose: GlobalSymbol.dispose ?? (GlobalSymbol.for('dispose') as any),
-  asyncDispose: GlobalSymbol.asyncDispose ?? (GlobalSymbol.for('asyncDispose') as any),
-  toFunction: '__toFunction__', // GlobalSymbol.for('w5s.toFunction') as SymbolConstructor['toFunction'],
+  iterator: __symbolAlias('iterator'),
+  asyncIterator: __symbolAlias('asyncIterator'),
+  hasInstance: __symbolAlias('hasInstance'),
+  isConcatSpreadable: __symbolAlias('isConcatSpreadable'),
+  match: __symbolAlias('match'),
+  replace: __symbolAlias('replace'),
+  search: __symbolAlias('search'),
+  species: __symbolAlias('species'),
+  split: __symbolAlias('split'),
+  toStringTag: __symbolAlias('toStringTag'),
+  toPrimitive: __symbolAlias('toPrimitive'),
+  unscopables: __symbolAlias('unscopables'),
+  matchAll: __symbolAlias('matchAll'),
+  dispose: __symbolAlias('dispose'),
+  asyncDispose: __symbolAlias('asyncDispose'),
+  toFunction: '__toFunction__', // TODO: GlobalSymbol.for('w5s.toFunction'),
   enumKeys: GlobalSymbol.for('w5s.enumKeys'),
   nodeInspect: GlobalSymbol.for('nodejs.util.inspect.custom'),
 };
