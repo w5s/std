f8667057b215609218b89366284026028927178d
Julien Polo <julien.polo@gmail.com>
2025-01-28 18:53:27 +0100

    ♻️ Use Task.ignore

--- packages/log/src/Log/handle.ts
+++ packages/log/src/Log/handle.ts
@@ -1,9 +1,8 @@
 import { all as taskAll } from '@w5s/task/dist/Task/all.js';
-import { map as taskMap } from '@w5s/task/dist/Task/map.js';
 import { from as taskFrom } from '@w5s/task/dist/Task/from.js';
 import { andThen as taskThen } from '@w5s/task/dist/Task/andThen.js';
+import { ignore as taskIgnore } from '@w5s/task/dist/Task/ignore.js';
 import type { Task } from '@w5s/task';
-import { ignore } from '@w5s/core/dist/ignore.js';
 import type { LogRecord } from '../LogRecord.js';
 import { LogApplication } from './LogApplication.js';
 
@@ -17,5 +16,5 @@ function getHandleTasks(logRecord: LogRecord): Task<Array<Task<void, never>>, ne
 export function handle(logRecord: LogRecord): Task<void, never> {
   const tasks = getHandleTasks(logRecord);
   const allTasks = taskThen(tasks, (_) => taskAll(_));
-  return taskMap(allTasks, ignore);
+  return taskIgnore(allTasks);
 }
