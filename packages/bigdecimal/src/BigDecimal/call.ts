64644ec7ce90513a9efa0fe1268c3e3b333e2157
Julien Polo <julien.polo@gmail.com>
2025-03-28 14:27:43 +0100

    ♻️ Rename throwError to panic

--- packages/bigdecimal/src/BigDecimal/call.ts
+++ packages/bigdecimal/src/BigDecimal/call.ts
@@ -1,4 +1,4 @@
-import { throwError } from '@w5s/error/dist/throwError.js';
+import { panic } from '@w5s/error/dist/panic.js';
 import { of } from './of.js';
 import { parse } from './parse.js';
 import type { BigDecimalString } from '../BigDecimal.js';
@@ -8,6 +8,6 @@ export function call(stringValue: BigDecimalString): BigDecimal;
 export function call(value: bigint, scale?: number): BigDecimal;
 export function call(value: string | bigint, scale?: number): BigDecimal {
   return typeof value === 'string'
-    ? (parse(value) ?? throwError(new TypeError(`${String(value)} is not a valid BigDecimal`)))
+    ? (parse(value) ?? panic(new TypeError(`${String(value)} is not a valid BigDecimal`)))
     : of(value, scale ?? 0);
 }
