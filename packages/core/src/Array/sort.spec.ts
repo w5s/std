5adaaebdf2e44815832cb60cefd3a745d0d1b0ee
Julien Polo <julien.polo@gmail.com>
2025-05-15 00:38:57 +0200

    ♻️ Use Ordering type for compareFn

--- packages/core/src/Array/sort.spec.ts
+++ packages/core/src/Array/sort.spec.ts
@@ -1,15 +1,16 @@
 import { describe, it, expect } from 'vitest';
 import { sort } from './sort.js';
 import { empty } from './empty.js';
+import { Number } from '../Number.js';
 
 describe(sort, () => {
   it('should return unchanged if empty', () => {
     const emptyArray = empty();
-    expect(sort(emptyArray, (left, right) => left - right)).toBe(emptyArray);
+    expect(sort(emptyArray, Number.compare)).toBe(emptyArray);
   });
   it('should map each value to callback', () => {
     const array = [11, 4, 6, 2];
 
-    expect(sort(array, (left, right) => left - right)).toEqual([2, 4, 6, 11]);
+    expect(sort(array, Number.compare)).toEqual([2, 4, 6, 11]);
   });
 });
--- packages/core/src/Array/sort.ts
+++ packages/core/src/Array/sort.ts
@@ -1,17 +1,18 @@
 import type { Array } from '../Array.js';
+import type { Ordering } from '../Ordering.js';
 
 /**
  * Return a sorted array using `compareFn`
  *
  * @example
  * ```typescript
- * Array.sort([11, 2, 22, 1], (a, b) => a - b);// [1, 2, 11, 22]
+ * Array.sort([11, 2, 22, 1], Number.compare);// [1, 2, 11, 22]
  * ```
  * @param array - The array object
  * @param compareFn - Function used to determine the order of the items. It is expected to return
  * a negative value if first argument is less than second argument, zero if they're equal and a positive
  * value otherwise.
  */
-export function sort<Item>(array: Array<Item>, compareFn: (a: Item, b: Item) => number) {
+export function sort<Item>(array: Array<Item>, compareFn: (a: Item, b: Item) => Ordering) {
   return array.length === 0 ? array : array.slice().sort(compareFn);
 }
