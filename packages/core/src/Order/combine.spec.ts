6716c687faa4161a4973f39472c2a0f6e8e45725
Julien Polo <julien.polo@gmail.com>
2025-05-20 00:16:29 +0200

    ✅ Use withOrder in tests

--- packages/core/src/Order/combine.spec.ts
+++ packages/core/src/Order/combine.spec.ts
@@ -2,9 +2,10 @@ import { describe, expect, it } from 'vitest';
 import { combine } from './combine.js';
 import { String } from '../String.js';
 import { Number } from '../Number.js';
-import { Ordering } from '../Ordering.js';
+import { withOrder } from '../Testing.js';
 
 describe(combine, () => {
+  const expectOrder = withOrder(expect);
   interface Person {
     lastName: string;
     firstName: string;
@@ -17,15 +18,13 @@ describe(combine, () => {
       (left: Person, right: Person) => Number.compare(left.age, right.age),
     );
 
-    expect(compare({ lastName: '', firstName: 'Alice', age: 25 }, { lastName: '', firstName: 'Bob', age: 30 })).toBe(
-      Ordering.Less,
-    );
-    expect(compare({ lastName: '', firstName: 'Alice', age: 25 }, { lastName: '', firstName: 'Alice', age: 25 })).toBe(
-      Ordering.Equal,
-    );
-    expect(compare({ lastName: '', firstName: 'Alice', age: 25 }, { lastName: '', firstName: 'Alice', age: 26 })).toBe(
-      Ordering.Less,
-    );
+    expectOrder(compare).toSortValues([
+      { lastName: 'A', firstName: 'Alice', age: 25 },
+      { lastName: 'B', firstName: 'Alice', age: 25 },
+      { lastName: 'B', firstName: 'Alice', age: 26 },
+      { lastName: 'A', firstName: 'Bob', age: 25 },
+      { lastName: 'B', firstName: 'Bob', age: 25 },
+    ]);
   });
   it('combine three orders', () => {
     const compare = combine(
@@ -33,10 +32,12 @@ describe(combine, () => {
       (left: Person, right: Person) => String.compare(left.firstName, right.firstName),
       (left: Person, right: Person) => Number.compare(left.age, right.age),
     );
-    const self = { lastName: 'B', firstName: 'Alice', age: 25 };
-
-    expect(compare(self, self)).toBe(Ordering.Equal);
-    expect(compare({ ...self, lastName: 'A' }, { ...self, lastName: 'B' })).toBe(Ordering.Less);
-    expect(compare({ ...self, firstName: 'A' }, { ...self, firstName: 'B' })).toBe(Ordering.Less);
+    expectOrder(compare).toSortValues([
+      { lastName: 'A', firstName: 'Alice', age: 25 },
+      { lastName: 'B', firstName: 'Alice', age: 25 },
+      { lastName: 'B', firstName: 'Alice', age: 26 },
+      { lastName: 'B', firstName: 'Bob', age: 25 },
+      { lastName: 'B', firstName: 'Bob', age: 25 },
+    ]);
   });
 });
--- packages/core/src/Order/reverse.spec.ts
+++ packages/core/src/Order/reverse.spec.ts
@@ -1,15 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import { reverse } from './reverse.js';
 import { NumberComparable } from '../Number/NumberComparable.js';
-import { Ordering } from '../Ordering.js';
+import { withOrder } from '../Testing.js';
 
 describe(reverse, () => {
+  const expectOrder = withOrder(expect);
+
   it('inverts less and greater', () => {
-    expect(reverse(NumberComparable.compare)(1, 1)).toBe(Ordering.Equal);
-    expect(reverse(NumberComparable.compare)(1, 2)).toBe(Ordering.Greater);
-    expect(reverse(NumberComparable.compare)(2, 1)).toBe(Ordering.Less);
+    expectOrder(reverse(NumberComparable.compare)).toSortValues([3, 2, 1]);
   });
   it('uses .compare for Comparable', () => {
-    expect(reverse(NumberComparable)(1, 2)).toBe(Ordering.Greater);
+    expectOrder(reverse(NumberComparable)).toSortValues([3, 2, 1]);
   });
 });
