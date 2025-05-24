cb276d20e8e49b9297ed55a940bcf3333ca8787e
Julien Polo <julien.polo@gmail.com>
2025-05-24 21:56:12 +0200

    ♻️ Refactor truncate options

--- packages/core/src/String/truncate.spec.ts
+++ packages/core/src/String/truncate.spec.ts
@@ -3,21 +3,27 @@ import { truncate } from './truncate.js';
 
 describe(truncate, () => {
   it('handles empty strings', () => {
-    expect(truncate('', 0)).toBe('');
-    expect(truncate('', 1)).toBe('');
+    expect(truncate('', { maxLength: 0 })).toBe('');
+    expect(truncate('', { maxLength: 1 })).toBe('');
   });
   it('keeps original string if it is shorter than the length', () => {
-    expect(truncate('abc', 3, { ellipsis: '...' })).toBe('abc');
+    expect(truncate('abc', { maxLength: 3, ellipsis: '...' })).toBe('abc');
   });
   it('handles length', () => {
-    expect(truncate('abc', 1, { ellipsis: '...' })).toBe('...');
-    expect(truncate('abc', 2, { ellipsis: '...' })).toBe('...');
+    expect(truncate('abc', { maxLength: 1, ellipsis: '...' })).toBe('...');
+    expect(truncate('abc', { maxLength: 2, ellipsis: '...' })).toBe('...');
   });
   it('handles custom ellipsis', () => {
-    expect(truncate('abc', 1, { ellipsis: 'XX' })).toBe('XX');
+    expect(truncate('abc', { maxLength: 1, ellipsis: 'XX' })).toBe('XX');
   });
   it('truncates string to specified length', () => {
-    expect(truncate('Hello World', 4)).toBe('H...');
-    expect(truncate('Hello World', 5)).toBe('He...');
+    expect(truncate('Hello World', { maxLength: 4 })).toBe('H...');
+    expect(truncate('Hello World', { maxLength: 5 })).toBe('He...');
+  });
+  it('truncates to 30 characters by default', () => {
+    expect(truncate('Hello World')).toBe('Hello World');
+    expect(truncate('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum.')).toBe(
+      'Lorem ipsum dolor sit amet,...',
+    );
   });
 });
--- packages/core/src/String/truncate.ts
+++ packages/core/src/String/truncate.ts
@@ -1,18 +1,23 @@
+import type { Int } from '../Int.js';
+
 /**
  * Truncates a string to a specified length, adding '...' if necessary.
  *
  * @example
  * ```typescript
- * truncate('Hello World', 1); // 'Hello World'
- * truncate('Hello World', 5); // 'He...'
- * truncate('Hello World', 5,  { ellipsis: '[...]' }); // 'He[...]'
+ * truncate('Hello World', { maxLength: 1 }); // 'Hello World'
+ * truncate('Hello World', { maxLength: 5 }); // 'He...'
+ * truncate('Hello World', { maxLength: 5, ellipsis: '[...]' }); // 'He[...]'
  * ```
  * @param self - The input string to truncate. If not provided or null/undefined,
  *                           will return the default truncation string ('...')
- * @param maxLength - The desired length of the output string
+ * @param options - An optional object containing additional options for the truncation.
  */
-export function truncate(self: string, maxLength: number, options: truncate.Options = truncate.defaultOptions): string {
-  const { ellipsis = truncate.defaultOptions.ellipsis } = options;
+export function truncate(self: string, options: truncate.Options = truncate.defaultOptions): string {
+  const { ellipsis, maxLength } = {
+    ...truncate.defaultOptions,
+    ...options,
+  };
   if (self.length > maxLength) {
     const end = maxLength - ellipsis.length;
     const truncated = self.slice(0, end);
@@ -26,9 +31,17 @@ export function truncate(self: string, maxLength: number, options: truncate.Opti
 }
 export namespace truncate {
   export type Options = {
+    /**
+     * A string to append at the end of the truncated string if it is shorter than the `maxLength`
+     */
     ellipsis?: string;
+    /**
+     * The desired length of the output string
+     */
+    maxLength?: number;
   };
   export const defaultOptions = {
     ellipsis: '...',
+    maxLength: 30 as Int,
   } satisfies Options;
 }
