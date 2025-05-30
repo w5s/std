fb9ccb406f6d9b16e228039024d8aaa71c87e70f
Julien Polo <julien.polo@gmail.com>
2025-05-30 23:56:00 +0200

    📝 Fix tsdoc

--- packages/money/src/Money/MoneySigned.ts
+++ packages/money/src/Money/MoneySigned.ts
@@ -1,10 +1,25 @@
-import type { Numeric } from '@w5s/core';
 import { BigDecimalSigned } from '@w5s/bigdecimal/dist/BigDecimal/BigDecimalSigned.js';
-import { Money } from './Money.js';
+import type { Money } from './Money.js';
 
 const { isPositive: bigDecimalIsPositive, isNegative: bigDecimalIsNegative } = BigDecimalSigned;
+interface MoneySigned {
+  /**
+   * Returns true if the number is positive and false if the number is zero or negative.
+   *
+   * @category Numeric
+   * @param self - the numeric value
+   */
+  isPositive(this: void, self: Money): boolean;
+  /**
+   * Returns true if the number is negative and false if the number is zero or positive.
+   *
+   * @category Numeric
+   * @param self - the numeric value
+   */
+  isNegative(this: void, self: Money): boolean;
+}
 
-export const MoneySigned: Pick<Numeric.Signed<Money>, 'isNegative' | 'isPositive'> = {
+export const MoneySigned: MoneySigned = {
   isPositive: ({ amount }) => bigDecimalIsPositive(amount),
   isNegative: ({ amount }) => bigDecimalIsNegative(amount),
 };
--- packages/money/src/Money/MoneyZero.ts
+++ packages/money/src/Money/MoneyZero.ts
@@ -1,9 +1,23 @@
-import type { Zero } from '@w5s/core/dist/Numeric/Zero.js';
 import { BigDecimalZero } from '@w5s/bigdecimal/dist/BigDecimal/BigDecimalZero.js';
 import type { Money } from './Money.js';
 
 const { isZero: bigDecimalIsZero } = BigDecimalZero;
 
-export const MoneyZero: Pick<Zero<Money>, 'isZero'> = {
+interface MoneyZero {
+  /**
+   * Returns true if self is equal to the additive identity.
+   *
+   * @example
+   * ```typescript
+   * Money.isZero(EUR('0')); // true
+   * Money.isZero(EUR('1')); // false
+   * ```
+   * @category Numeric
+   * @param self - the value to test
+   */
+  isZero(this: void, self: Money): boolean;
+}
+
+export const MoneyZero: MoneyZero = {
   isZero: ({ amount }) => bigDecimalIsZero(amount),
 };
