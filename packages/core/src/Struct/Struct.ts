d69b8888aa86b02c12208506836e3a5c13d089b1
Julien Polo <julien.polo@gmail.com>
2025-06-10 19:07:05 +0200

    ✨ Set Struct tag string as Object
    Reason : better display in inspect and snapshot

--- packages/core/src/Struct/Struct.ts
+++ packages/core/src/Struct/Struct.ts
@@ -2,12 +2,23 @@ import type { AsString } from '../AsString.js';
 import { compare } from '../String/compare.js';
 import type { InspectFunction, InspectOptions, Type } from '../Type.js';
 
+const {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  Object: { assign },
+  String: toString,
+} = globalThis;
 const defaultSort = (left: string | symbol, right: string | symbol) =>
-  left === right ? 0 : left === '_' ? -1 : right === '_' ? 1 : compare(String(left), String(right));
+  left === right ? 0 : left === '_' ? -1 : right === '_' ? 1 : compare(toString(left), String(right));
 
-export class Struct {
-  static create<T>(mod: Type<T> & AsString<T>, properties: T): Struct & T {
-    return Object.assign(new Struct(mod), properties);
+/**
+ * @internal
+ * @param mod - the module (or TS namespace)
+ */
+export const Struct = class Object {
+  // Important : we name Object so it is shown as anonymous object in snapshot.
+
+  static create<T>(mod: Type<T> & AsString<T>, properties: T): T {
+    return assign(new Struct(mod), properties);
   }
 
   #module: Type<unknown> & AsString<unknown>;
@@ -41,4 +52,4 @@ export class Struct {
   toString() {
     return this.#module.asString(this);
   }
-}
+};
