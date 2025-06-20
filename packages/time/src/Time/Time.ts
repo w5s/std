00ccfc88ea81b65e18daadf75ab9a2b02b48e4e2
Julien Polo <julien.polo@gmail.com>
2025-06-20 15:22:14 +0200

    ♻️ Simplify compare implementation

--- packages/time/src/Time/Time.ts
+++ packages/time/src/Time/Time.ts
@@ -1,16 +1,6 @@
 import { Tag } from '@w5s/core/dist/Tag.js';
 import { Symbol } from '@w5s/core/dist/Symbol.js';
-import type { Codec } from '@w5s/core';
 import { parse } from './parse.js';
-
-const TimeCodec: Codec<Time> = {
-  [Symbol.encode]: (input) => new Date(input).toISOString(),
-  [Symbol.decode]: (input, { ok, error }) => {
-    const timestamp = typeof input === 'string' ? parse(input) : undefined;
-    return timestamp == null ? error(input, 'Time') : ok(timestamp);
-  },
-  [Symbol.schema]: () => ({ type: 'string', format: 'date-time' }),
-};
 
 /**
  * Represent a time typically returned by `Date.now()`
@@ -22,5 +12,10 @@ export const Time = Tag.define<number, Time>({
   hasInstance(anyValue) {
     return typeof anyValue === 'number' && anyValue >= -8.64e15 && anyValue <= 8.64e15;
   },
-  ...TimeCodec,
+  [Symbol.encode]: (input) => new Date(input).toISOString(),
+  [Symbol.decode]: (input, { ok, error }) => {
+    const timestamp = typeof input === 'string' ? parse(input) : undefined;
+    return timestamp == null ? error(input, 'Time') : ok(timestamp);
+  },
+  [Symbol.schema]: () => ({ type: 'string', format: 'date-time' }),
 });
--- packages/time/src/TimeDuration/TimeDurationComparable.ts
+++ packages/time/src/TimeDuration/TimeDurationComparable.ts
@@ -1,7 +1,7 @@
 import { Comparable } from '@w5s/core/dist/Comparable.js';
-import { compare } from '@w5s/num/dist/Number/compare.js';
+import { compare } from './compare.js';
 import type { TimeDuration } from './TimeDuration.js';
 
 export const TimeDurationComparable = Comparable<TimeDuration>({
-  compare: compare as Comparable<TimeDuration>['compare'],
+  compare,
 });
