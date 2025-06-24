f563e64d1cb9e1f32df2551b2f692ac78d4fc92d
Julien Polo <julien.polo@gmail.com>
2025-06-24 18:09:01 +0200

    ✅ Simplify tests

--- packages/bigdecimal/src/BigDecimal/BigDecimalNumeric.spec.ts
+++ packages/bigdecimal/src/BigDecimal/BigDecimalNumeric.spec.ts
@@ -5,34 +5,20 @@ import { BigDecimalComparable } from './BigDecimalComparable.js';
 import { BigDecimal } from './BigDecimal.js';
 
 describe('BigDecimalNumeric', () => {
-  describeAdd(
-    {
-      ...BigDecimalComparable,
-      ...BigDecimalNumeric,
-    },
-    [
-      { call: [BigDecimal('0'), BigDecimal('0')], returns: BigDecimal('0') },
-      { call: [BigDecimal('1.01'), BigDecimal('2.2')], returns: BigDecimal('3.21') },
-    ],
-  );
-  describeSubtract(
-    {
-      ...BigDecimalComparable,
-      ...BigDecimalNumeric,
-    },
-    [
-      { call: [BigDecimal('0'), BigDecimal('0')], returns: BigDecimal('0') },
-      { call: [BigDecimal('2.5'), BigDecimal('0.01')], returns: BigDecimal('2.49') },
-    ],
-  );
-  describeMultiply(
-    {
-      ...BigDecimalComparable,
-      ...BigDecimalNumeric,
-    },
-    [
-      { call: [BigDecimal('0'), BigDecimal('0')], returns: BigDecimal('0') },
-      { call: [BigDecimal('2.2'), BigDecimal('0.5')], returns: BigDecimal('1.10') },
-    ],
-  );
+  const subject = {
+    ...BigDecimalComparable,
+    ...BigDecimalNumeric,
+  };
+  describeAdd(subject, [
+    { call: [BigDecimal('0'), BigDecimal('0')], returns: BigDecimal('0') },
+    { call: [BigDecimal('1.01'), BigDecimal('2.2')], returns: BigDecimal('3.21') },
+  ]);
+  describeSubtract(subject, [
+    { call: [BigDecimal('0'), BigDecimal('0')], returns: BigDecimal('0') },
+    { call: [BigDecimal('2.5'), BigDecimal('0.01')], returns: BigDecimal('2.49') },
+  ]);
+  describeMultiply(subject, [
+    { call: [BigDecimal('0'), BigDecimal('0')], returns: BigDecimal('0') },
+    { call: [BigDecimal('2.2'), BigDecimal('0.5')], returns: BigDecimal('1.10') },
+  ]);
 });
