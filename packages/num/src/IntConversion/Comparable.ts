229bd46928cfda2ba3ddf3265ed3b4979cf37ceb
Julien Polo <julien.polo@gmail.com>
2026-01-30 18:54:25 +0100

    ♻️ Remove code duplication

--- packages/num/src/IntConversion/Comparable.ts
+++ packages/num/src/IntConversion/Comparable.ts
@@ -1,14 +1,12 @@
-import { Comparable as ComparableModule } from '@w5s/core/dist/Comparable.js';
+import type { Comparable as ComparableModule } from '@w5s/core';
+import { Comparable as NumberComparable } from '../NumberConversion/Comparable.js';
 import type { Int } from '../Int.js';
-import { compare } from '../Int/compare.js';
 import type { IntConversion } from '../IntConversion.js';
 import { __defaultConversion } from './__defaultConversion.js';
+import { __toNumberConversion } from './__toNumberConversion.js';
 
-export function Comparable<T = Int>(IntLikeType?: Pick<IntConversion<T>, 'asInt'>): ComparableModule<T> {
-  const { asInt } = IntLikeType ?? __defaultConversion();
-  return ComparableModule<T>({
-    compare(left, right) {
-      return compare(asInt(left), asInt(right));
-    },
-  });
+export function Comparable(): ComparableModule<Int>;
+export function Comparable<T>(BaseType: IntConversion<T>): ComparableModule<T>;
+export function Comparable<T>(BaseType?: IntConversion<T>): ComparableModule<T> {
+  return NumberComparable(__toNumberConversion(BaseType ?? __defaultConversion()));
 }
--- packages/num/src/IntConversion/Negate.ts
+++ packages/num/src/IntConversion/Negate.ts
@@ -1,14 +1,12 @@
 import type { Numeric } from '@w5s/core';
 import type { IntConversion } from '../IntConversion.js';
+import { Negate as NumberNegate } from '../NumberConversion/Negate.js';
 import { __defaultConversion } from './__defaultConversion.js';
+import { __toNumberConversion } from './__toNumberConversion.js';
+import type { Int } from '../Int.js';
 
-export function Negate<T>(IntLikeType?: IntConversion<T>): Numeric.Negate<T> {
-  const { fromInt, asInt } = IntLikeType ?? __defaultConversion();
-  return {
-    negate(self) {
-      // @ts-ignore we know asInt(self) is Int
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-unary-minus
-      return fromInt(-asInt(self));
-    },
-  };
+export function Negate(): Numeric.Negate<Int>;
+export function Negate<T>(BaseType: IntConversion<T>): Numeric.Negate<T>;
+export function Negate<T>(BaseType?: IntConversion<T>): Numeric.Negate<T> {
+  return NumberNegate(__toNumberConversion(BaseType ?? __defaultConversion()));
 }
