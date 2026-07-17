93206a36b602b5c2d40b0237c3c52b1a662af1c7
Julien Polo <julien.polo@gmail.com>
2026-07-17 17:40:07 +0200

    Squashed '.modules/makefile-ci/' changes from aa56951a3b..fd358c8b3f
    fd358c8b3f ♻️ Optimize CI variable assignments with memoization
    820f739963 🐛 Add memoization function to optimize expensive value computations
    372ccaa400 🐛 Rollback performance regression
    3463334e48 🐛 Fix version retrieval from package.json in version.mk
    b168d359bc 🐛 Fix assignment operator for CI variables in variables.mk
    c97c81f45e 🐛 Remove package.json file for performances and better resolution from lockfile
    ed40cd653a 🐛 Remove redundant installation targets for Pod, Node, and Ruby dependencies
    0f895be9b9 Merge pull request #30 from w5s/renovate/ubuntu-26.04
    2576492e98 🔖 Upgrade ubuntu:26.04 Docker digest to b7f4819
    
    git-subtree-dir: .modules/makefile-ci
    git-subtree-split: fd358c8b3f3dec2d4a6b47cbfa5d68612a5f5553

--- src/node.mk
+++ src/node.mk
@@ -144,9 +144,8 @@ $(NODEJS_CACHE_PATH)/node-version: $(NODEJS_CACHE_PATH)
 	$(Q)echo $(NODEJS_VERSION) > $@
 
 # A target that will run node install only if lockfile was changed
-NODEJS_PACKAGE_JSON_FILES := $(shell find . -name package.json -not -path '**/node_modules/*' -not -path './.git/*')
 
-$(NODEJS_STATEFILE): $(wildcard $(NODEJS_LOCKFILE)) $(NODEJS_PACKAGE_JSON_FILES)
+$(NODEJS_STATEFILE): $(wildcard $(NODEJS_LOCKFILE))
 # Try installing package manager
 ifeq ($(filter npm,$(NODEJS_PACKAGE_MANAGER)),)
 # Only for asdf we have to reshim after corepack
@@ -204,17 +203,6 @@ ifneq ($(filter $(NODEJS_PACKAGE_MANAGER),npm),)
 	fi
 endif
 .setup:: node-setup # Add to `make setup`
-
-#
-# Install node dependencies (ex: npm install)
-#
-# This will install dependencies using $(NODEJS_PACKAGE_MANAGER). In CI mode, the lockfile update will be forbidden.
-#
-.PHONY: node-install
-node-install: node-setup
-	$(Q)$(RM) -f $(NODEJS_STATEFILE)
-	$(Q)$(MAKE) node-dependencies
-.install:: node-install	# Add `npm install` to `make install`
 
 #
 # Run npm lint script (ex: npm run lint)
